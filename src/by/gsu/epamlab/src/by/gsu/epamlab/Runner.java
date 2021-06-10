ea0e818529ff78682f3e6be6cb4f047bc6112bb8
ABonokhov <43575362+ABonokhov@users.noreply.github.com>
2021-06-10 21:16:22 +0300

    Finish_comm4

---
+++ src/by/gsu/epamlab/src/by/gsu/epamlab/Runner.java
@@ -0,0 +1,49 @@
+package by.gsu.epamlab;
+import java.util.Arrays;
+import static by.gsu.epamlab.Employee.printRuble;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        Employee[] employees = {
+                new Employee( "Karim Bataev", 1234, 12),
+                new Employee( "Alexei Zareckiy", 1632, 11),
+                null,
+                new Employee( "Alesya Chernitskaya", 985, 7),
+                new Employee(),
+        };
+
+        int max = 0;
+        for (Employee employee : employees) {    // cycle for calculate business trip with maximum cost
+            if (employee != null && employee.getTotal() > max) {
+                max = employee.getTotal();
+            }
+
+            if (employee != null) {
+                employee.show();   // using show( ) method for output array to the console
+                System.out.println("___________________");
+            }
+
+            employees[4].setTransport(1649);  // Update  transportation expenses for the last object of the array
+        }
+        System.out.println("Duration = " + (employees[0].getDays() + (employees[1].getDays()))); // Output the total duration of two initial business trips
+        System.out.println(Arrays.toString(employees));   //  Output the array content to the console using toString()
+        System.out.print("Maximum cost is  " + printRuble(max));
+    }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
