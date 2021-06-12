65733277bc13352f862040dc502f159e5c5f082e
ABonokhov <43575362+ABonokhov@users.noreply.github.com>
2021-06-12 18:45:06 +0300

    Classes1_finish

---
+++ src/Runner.java
@@ -0,0 +1,33 @@
+import by.gsu.epamlab.BusinessTrip;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        BusinessTrip[]  businessTrips = {
+                new BusinessTrip( "Karim Bataev", 1234, 12),
+                new BusinessTrip( "Alexei Zareckiy", 1632, 11),
+                null,
+                new BusinessTrip( "Alesya Chernitskaya", 985, 7),
+                new BusinessTrip(),
+        };
+
+        BusinessTrip maxTotal = new BusinessTrip();
+        for (BusinessTrip  businessTrip :  businessTrips) {
+            if (businessTrip != null) {
+                businessTrip.show(); // Output the array content to the console, using show( ) method
+                if (businessTrip.getTotal() > maxTotal.getTotal()) {
+                    maxTotal = businessTrip;
+                    maxTotal.show();  // Output the business trip with maximum cost using show() method
+                }
+            }
+        }
+        
+        businessTrips[businessTrips.length - 1].setExpenses(1649); // Update  transportation expenses for the last object of the array
+        System.out.println("Duration=" + (businessTrips[0].getDaysNumber() + businessTrips[1].getDaysNumber())); // Output the total duration of two initial business trips by the single operator
+
+        for (BusinessTrip businessTrip : businessTrips) {
+            System.out.println(businessTrip);
+        }
+    }
+}
+
---
+++ src/by/gsu/epamlab/BusinessTrip.java
@@ -0,0 +1,53 @@
+package by.gsu.epamlab;
+
+    public class BusinessTrip {
+        private static final int DAILY_RATE = 950; // daily allowance rate in BYN (constant)
+        private String account; // employee`s account
+        private int expenses; // transportation expenses in BYN
+        private int daysNumber; // number of days
+
+        public BusinessTrip() { // no-arg constructor
+            this(null,0,0);
+        }
+
+        public BusinessTrip(String account, int expenses, int daysNumber) {  // parameterized constructor
+            this.account = account;
+            this.expenses = expenses;
+            this.daysNumber= daysNumber;
+        }
+
+        public void setExpenses(int expenses) {
+            this.expenses = expenses;
+        } // getters and setters
+
+        public int getDaysNumber() {
+            return daysNumber;
+        }
+
+        public int getTotal() {
+            return expenses + DAILY_RATE * daysNumber;  // returns the total business trip expenses in BYN
+        }
+
+        public void show() {        // outputs all fields to the console
+            System.out.println("rate = " + printRuble(DAILY_RATE) + ", " +  "account = " + account + ", " +  "transport = " +  printRuble(expenses) + ", " + "days = " + daysNumber + ", " + "total = " + printRuble(getTotal()));
+        }
+
+        @Override
+        public String toString() {    // returns a string representation of a business trip in the csv–format
+            return account + ";" + printRuble(expenses) + ";" + daysNumber + ";" + printRuble(getTotal());
+        }
+
+        static String printRuble(int sum) {       // Method for formatting money
+            return String.format("%d.%02d", sum / 100, sum % 100);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
