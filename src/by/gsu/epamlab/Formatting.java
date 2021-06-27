d41620fa8686868d80d83a7427bc7f1275a617de
ABonokhov <43575362+ABonokhov@users.noreply.github.com>
2021-06-27 23:07:48 +0300

    Delete Formatting.java

--- src/by/gsu/epamlab/Formatting.java
+++
@@ -1,9 +0,0 @@
-package by.gsu.epamlab;
-
-public class Formatting {
-
-    public static String printRuble(int sum) {    // Method for formatting money
-        return String.format("%d.%02d", sum / 100, sum % 100);
-    }
-
-}
